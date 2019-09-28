2cb8038673c6df0e714ba384f1658e6b79469d95
Donald Lika <donaldlika@hotmail.com>
2019-09-28 16:53:52 +0200

    Add handlers for alexa translate, include constants/env files

---
+++ lambda/constants.js
@@ -0,0 +1,22 @@
+
+const states = {
+    SEARCHMODE: '_SEARCHMODE',
+};
+
+const welcomeMessage = "You can translate words in Albanian Language. Do you want me to translate anything?";
+
+const helpMeMessage = "Here are some things you can say: What does Evolution mean in albanian. Translate Box in albanian";
+
+const killMessage = "Ok, great, see you next time.";
+
+const shutdownMessage = "Ok see you again soon.";
+
+let outputMessage = "";
+
+module.export={
+    welcomeMessage,
+    helpMeMessage,
+    killMessage,
+    shutdownMessage,
+    outputMessage
+}
--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,82 @@
+import {welcomeMessage,outputMessage,helpMeMessage,states,killMessage,shutdownMessage} from './constants';
+
 const Alexa = require('alexa-sdk');
 
-exports.handler = function (event, context) {
-	context.succeed('Hello Alexa Lambda');
+let alexa;
+
+let APP_ID = process.env.APP_ID;
+
+const sessionHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.response.speak(skillName + " " + welcomeMessage).listen(welcomeMessage);
+        this.emit(':responseReady');
+    },
+    "searchIntent": function()
+    {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState("TranslateInAlbanianIntent");
+    },
+    'Unhandled': function () {
+        this.response.speak(HelpMeMessage).listen(HelpMeMessage);
+        this.emit(':responseReady');
+    },
+};
+
+const startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
+    'AMAZON.YesIntent': function () {
+        outputMessage = welcomeMessage;
+        alexa.response.speak(outputMessage).listen(welcomeMessage);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.NoIntent': function () {
+        this.response.speak(shutdownMessage);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.RepeatIntent': function () {
+        this.response.speak(outputMessage).listen(HelpMeMessage);
+    },
+    'TranslateInAlbanianIntent': function () {
+        const slotValue = this.event.request.intent.slots.QUERY.value;
+        if (slotValue != undefined)
+        {
+            //TODO Add implementation for Translation
+        }
+        else{
+            this.response.speak("I couldn't find what word do you want to Translate").listen("I couldn't find what word do you want to Translate");
+        }
+
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.HelpIntent': function () {
+        this.response.speak(HelpMeMessage).listen(HelpMeMessage);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.StopIntent': function () {
+        this.response.speak(killMessage);
+    },
+
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(killMessage);
+    },
+
+    'SessionEndedRequest': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+
+    'Unhandled': function () {
+        this.response.speak(HelpMeMessage).listen(HelpMeMessage);
+        this.emit(':responseReady');
+    }
+});
+
+exports.handler = function (event, context, callback) {
+    alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(sessionHandlers, startSearchHandlers);
+    alexa.execute();
 };
